bc697fc50008758105dc3f9967e9e4fba7da32a9
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-11-02 15:53:15 +0100

    Moving dir and updating run script

--- SHE_Pipeline/scripts/run_analysis_pipeline.sh
+++ SHE_Pipeline/scripts/run_analysis_pipeline.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
 
+echo password | sudo -S systemctl start euclid-ial-wfm
+pipeline_runner.py --configure --config=/etc/euclid-ial/euclid_prs_app.cfg --serverurl="http://localhost:50000"
+
 pipeline_runner.py --pipeline=analysis_pipeline.py --data=/home/user/Work/Projects/SHE_Pipeline/SHE_Pipeline/auxdir/SHE_Pipeline/analysis_isf.txt --serverurl="http://localhost:50000"
