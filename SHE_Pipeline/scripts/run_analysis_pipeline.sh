51b5bf94a0c8813d13922724fff00337cc4f3cb5
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-11-02 16:05:09 +0100

    Final bugfixes

--- SHE_Pipeline/scripts/run_analysis_pipeline.sh
+++ SHE_Pipeline/scripts/run_analysis_pipeline.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
-echo password | sudo -S systemctl start euclid-ial-wfm
+echo password | sudo -S systemctl restart euclid-ial-wfm
+sleep 1
 pipeline_runner.py --configure --config=/etc/euclid-ial/euclid_prs_app.cfg --serverurl="http://localhost:50000"
 
 pipeline_runner.py --pipeline=analysis_pipeline.py --data=/home/user/Work/Projects/SHE_Pipeline/SHE_Pipeline/auxdir/SHE_Pipeline/analysis_isf.txt --serverurl="http://localhost:50000"
