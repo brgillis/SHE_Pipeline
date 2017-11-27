f24c81d260e09ea0e0a030ce9739366ffd07e459
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-11-27 18:58:09 +0100

    Adding cleanup script

---
+++ SHE_Pipeline/scripts/cleanup_data.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+SCRIPTDIR=/home/user/Work/Projects/SHE_Pipeline/SHE_Pipeline/scripts
+
+source $SCRIPTDIR/set_pipeline_envvars.sh
+
+mkdir -p $WORKDIR
+mkdir -p $LOGDIR
+
+CMD="rm $WORKDIR/EUC*.bin $WORKDIR/*.fits $WORKDIR/*.json $LOGDIR/*"
+
+echo $CMD
+`$CMD`
+
