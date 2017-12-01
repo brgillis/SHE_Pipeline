1de90485f43871df1ae5749cdb049ec888f0f31d
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-12-01 17:27:46 +0100

    Updating arguments

--- scripts/set_analysis_envvars.sh
+++ scripts/set_analysis_envvars.sh
@@ -4,19 +4,22 @@
 export WORKDIR="/home/user/euclid-ial/workspace/tests/she_analysis"
 export LOGDIR=$WORKDIR"/logdir"
 
-export DATA_IM="sim_data_images.json"
-export PSF_CAL="mock_psf_calibration_products.json"
-export SEG_IM="mock_segmentation_products.json"
-export DAL_TAB="mock_details_tables.json"
-export DTC_TAB="mock_detections_tables.json"
-export AST_PROD="mock_astrometry_products.json"
-export AOCS_PROD="mock_aocs_time_series_products.json"
-export MT_PROD="mock_mission_time_products.json"
-export GP_TAB="mock_galaxy_population_priors_table.bin"
+# Input products/listfiles
+
+export DATA_IM_LF="sim_data_images.json"
+export PSF_CAL_LF="mock_psf_calibration_products.json"
+export SEG_IM_LF="mock_segmentation_products.json"
+export DAL_LF="mock_details_tables.json"
+export DTC_LF="mock_detections_tables.json"
+export AST_LF="mock_astrometry_products.json"
+export AOCS_LF="mock_aocs_time_series_products.json"
+export MT_LF="mock_mission_time_products.json"
+export GP_PROD="mock_galaxy_population_priors_table.bin"
 export CAL_PROD="mock_calibration_parameters_product.bin"
-export CAL_LF="mock_calibration_parameters_listfile.json"
-export SV_TAB="mock_shear_validation_statistics_table.bin"
-export PSF_IMTAB="mock_psf_images_and_tables.bin"
+export SV_PROD="mock_shear_validation_statistics_table.bin"
+
+# Intermediate products/listfiles
+
+export PSF_LF="mock_psf_images_and_tables.bin"
 export SE_PROD="mock_shear_estimates.bin"
-export SE_LF="mock_shear_estimates.json"
-export VSE_TAB="mock_validated_shear_estimates.bin"
+export VSE_PROD="mock_validated_shear_estimates.bin"
