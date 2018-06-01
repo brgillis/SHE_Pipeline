f44fd2830c155716f9b4a3c8f542f419fc52db56
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-06-01 13:49:30 +0100

    Updating with interface changes

--- scripts/run_manual_analysis_pipeline_sc3.sh
+++ scripts/run_manual_analysis_pipeline_sc3.sh
@@ -8,7 +8,7 @@ fi
 
 source $SCRIPTDIR/set_analysis_envvars_sc3.sh
 
-CMD="E-Run SHE_CTE 0.4 SHE_CTE_FitPSFs --data_images $DATA_IM_LF --detections_tables $DTC_LF --psf_images_and_tables $PSF_LF --workdir $WORKDIR --logdir $LOGDIR"
+CMD="E-Run SHE_CTE 0.3 SHE_CTE_FitPSFs --data_images $DATA_IM_LF --detections_tables $DTC_LF --psf_images_and_tables $PSF_LF --workdir $WORKDIR --logdir $LOGDIR"
 
 echo "Fitting PSFs with command:"
 echo $CMD
@@ -18,7 +18,7 @@ if [ $? -ne 0 ]; then
     exit
 fi
 
-CMD="E-Run SHE_CTE 0.4 SHE_CTE_EstimateShear --data_images $DATA_IM_LF --stacked_image $DATA_STACK_PROD --psf_images_and_tables $PSF_LF --segmentation_images $SEG_LF --stacked_segmentation_image $SEG_STACK_F --detections_tables $DTC_LF --shear_estimates_product $SE_PROD --workdir $WORKDIR --logdir $LOGDIR"
+CMD="E-Run SHE_CTE 0.3 SHE_CTE_EstimateShear --data_images $DATA_IM_LF --stacked_image $DATA_STACK_PROD --psf_images_and_tables $PSF_LF --segmentation_images $SEG_LF --stacked_segmentation_image $SEG_STACK_F --detections_tables $DTC_LF --galaxy_population_priors_table $GP_PROD --shear_estimates_product $SE_PROD --workdir $WORKDIR --logdir $LOGDIR"
 
 echo "Estimating shear with command:"
 echo $CMD
@@ -28,7 +28,7 @@ if [ $? -ne 0 ]; then
     exit
 fi
 
-CMD="E-Run SHE_CTE 0.4 SHE_CTE_ValidateShear --shear_estimates_product $SE_PROD --validated_shear_estimates_table $VSE_PROD --workdir $WORKDIR --logdir $LOGDIR"
+CMD="E-Run SHE_CTE 0.3 SHE_CTE_ValidateShear --shear_estimates_product $SE_PROD --cross_validated_shear_estimates_table $VSE_PROD --workdir $WORKDIR --logdir $LOGDIR"
 
 echo "Validating shear with command:"
 echo $CMD
