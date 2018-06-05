480bb6cef5a8b4718b7c48a68c5a15202cbecba7
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-06-05 13:38:29 +0200

    Updating normal run scripts

--- scripts/run_manual_analysis_pipeline.sh
+++ scripts/run_manual_analysis_pipeline.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/bash
 
-SCRIPTDIR=/home/user/Work/Projects/SHE_Pipeline/scripts
+if [ $# -gt 0 ]; then
+  SCRIPTDIR=$1
+else
+  SCRIPTDIR=/home/user/Work/Projects/SHE_Pipeline/scripts
+fi
 
 source $SCRIPTDIR/set_analysis_envvars.sh
 
-CMD="E-Run SHE_CTE 0.3 SHE_CTE_FitPSFs --data_images $DATA_IM_LF --detections_tables $DTC_LF --astrometry_products $AST_LF --aocs_time_series_products $AOCS_LF --mission_time_products $MT_LF --psf_calibration_products $PSF_CAL_LF --psf_images_and_tables $PSF_LF --workdir $WORKDIR --logdir $LOGDIR"
+CMD="E-Run SHE_CTE 0.3 SHE_CTE_FitPSFs --data_images $DATA_IM_LF --detections_tables $DTC_LF --psf_images_and_tables $PSF_LF --workdir $WORKDIR --logdir $LOGDIR"
 
 echo "Fitting PSFs with command:"
 echo $CMD
@@ -14,7 +18,7 @@ if [ $? -ne 0 ]; then
     exit
 fi
 
-CMD="E-Run SHE_CTE 0.3 SHE_CTE_EstimateShear --data_images $DATA_IM_LF --psf_images_and_tables $PSF_LF --segmentation_images $SEG_IM_LF --detections_tables $DTC_LF --galaxy_population_priors_table $GP_PROD --calibration_parameters_product $CAL_PROD --shear_estimates_product $SE_PROD --workdir $WORKDIR --logdir $LOGDIR"
+CMD="E-Run SHE_CTE 0.3 SHE_CTE_EstimateShear --data_images $DATA_IM_LF --stacked_image $DATA_STACK_PROD --psf_images_and_tables $PSF_LF --segmentation_images $SEG_LF --stacked_segmentation_image $SEG_STACK_F --detections_tables $DTC_LF --shear_estimates_product $SE_PROD --workdir $WORKDIR --logdir $LOGDIR"
 
 echo "Estimating shear with command:"
 echo $CMD
@@ -24,7 +28,7 @@ if [ $? -ne 0 ]; then
     exit
 fi
 
-CMD="E-Run SHE_CTE 0.3 SHE_CTE_CrossValidateShear --shear_estimates_product $SE_PROD --cross_validated_shear_estimates_table $CVSE_PROD --workdir $WORKDIR --logdir $LOGDIR"
+CMD="E-Run SHE_CTE 0.3 SHE_CTE_CrossValidateShear --shear_estimates_product $SE_PROD --cross_validated_shear_estimates_product $CVSE_PROD --workdir $WORKDIR --logdir $LOGDIR"
 
 echo "Validating shear with command:"
 echo $CMD
