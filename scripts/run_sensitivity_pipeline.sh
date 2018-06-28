6c739a14b136e1bd95c7322d1d98c7906f6db7f0
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-06-28 15:12:10 +0100

    Fixing ISF directories

--- scripts/run_sensitivity_pipeline.sh
+++ scripts/run_sensitivity_pipeline.sh
@@ -4,4 +4,4 @@ echo password | sudo -S systemctl restart euclid-ial-wfm
 sleep 1
 pipeline_runner.py --configure --config=/etc/euclid-ial/euclid_prs_app.cfg --serverurl="http://localhost:50000"
 
-pipeline_runner.py --pipeline=sensitivity_pipeline.py --data=/home/user/Work/Projects/SHE_Pipeline/SHE_Pipeline/pkgdef/sensitivity_isf.txt --serverurl="http://localhost:50000"
+pipeline_runner.py --pipeline=sensitivity_pipeline.py --data=/home/user/Work/Projects/SHE_Pipeline/SHE_Pipeline/params/sensitivity_isf.txt --serverurl="http://localhost:50000"
