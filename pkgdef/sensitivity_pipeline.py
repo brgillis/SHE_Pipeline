31b262bf7f480863184214cb6c79441a172a8a6c
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-06-28 15:15:08 +0100

    Need to import parallel

--- pkgdef/sensitivity_pipeline.py
+++ pkgdef/sensitivity_pipeline.py
@@ -20,7 +20,7 @@ __updated__ = "2018-06-28"
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-from euclidwf.framework.workflow_dsl import pipeline
+from euclidwf.framework.workflow_dsl import pipeline, parallel
 from pkgdef.package_definition import (she_prepare_configs, she_simulate_images, she_estimate_shear,
                                        she_measure_statistics, she_measure_bias, she_compile_statistics)
 
