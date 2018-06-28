5dd39fa8f0cd387f5c37bf938b3bd4955f02f359
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-06-28 15:10:53 +0100

    Adding run script for sensitivity pipeline

---
+++ scripts/run_sensitivity_pipeline.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+echo password | sudo -S systemctl restart euclid-ial-wfm
+sleep 1
+pipeline_runner.py --configure --config=/etc/euclid-ial/euclid_prs_app.cfg --serverurl="http://localhost:50000"
+
+pipeline_runner.py --pipeline=sensitivity_pipeline.py --data=/home/user/Work/Projects/SHE_Pipeline/SHE_Pipeline/pkgdef/sensitivity_isf.txt --serverurl="http://localhost:50000"
