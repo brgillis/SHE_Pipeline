945d5736bbd426b3a4a0789e3e80c2874c4f9640
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-07-03 16:31:12 +0100

    Pointing to custom cfg

--- scripts/run_analysis_pipeline.sh
+++ scripts/run_analysis_pipeline.sh
@@ -2,6 +2,6 @@
 
 echo password | sudo -S systemctl restart euclid-ial-wfm
 sleep 1
-pipeline_runner.py --configure --config=/etc/euclid-ial/euclid_prs_app.cfg --serverurl="http://localhost:50000"
+pipeline_runner.py --configure --config=/home/user/Work/Projects/SHE_Pipeline/euclid_prs_app.cfg --serverurl="http://localhost:50000"
 
 pipeline_runner.py --pipeline=analysis_pipeline.py --data=/home/user/Work/Projects/SHE_Pipeline/params/analysis_isf.txt --serverurl="http://localhost:50000"
