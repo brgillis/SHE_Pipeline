7aa385adf803c21a1f810f5525d9d4c9752b1051
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-07-04 14:01:10 +0100

    Sorting files so they'll be deployed properly

--- scripts/run_analysis_pipeline.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-echo password | sudo -S systemctl restart euclid-ial-wfm
-sleep 1
-pipeline_runner.py --configure --config=/home/user/Work/Projects/SHE_Pipeline/euclid_prs_app.cfg --serverurl="http://localhost:50000"
-
-pipeline_runner.py --pipeline=analysis_pipeline.py --data=/home/user/Work/Projects/SHE_Pipeline/params/analysis_isf.txt --serverurl="http://localhost:50000"
