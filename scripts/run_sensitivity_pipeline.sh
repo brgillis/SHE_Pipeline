a9009d013fb23b1f8eddcabf9a50621c96006566
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-07-04 13:30:24 +0100

    Updating files for CVMFS deployment

--- scripts/run_sensitivity_pipeline.sh
+++ scripts/run_sensitivity_pipeline.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-echo password | sudo -S systemctl restart euclid-ial-wfm
-sleep 1
-pipeline_runner.py --configure --config=/home/user/Work/Projects/SHE_Pipeline/euclid_prs_app.cfg --serverurl="http://localhost:50000"
+# echo password | sudo -S systemctl restart euclid-ial-wfm
+# sleep 1
 
-pipeline_runner.py --pipeline=sensitivity_pipeline.py --data=/home/user/Work/Projects/SHE_Pipeline/params/sensitivity_isf.txt --serverurl="http://localhost:50000"
+pipeline_runner.py --configure --config=/cvmfs/euclid-dev.in2p3.fr/CentOS7/EDEN-2.0/opt/euclid/SHE_Pipeline/euclid_prs_app.cfg --serverurl="http://localhost:50000"
+pipeline_runner.py --pipeline=sensitivity_pipeline.py --data=/cvmfs/euclid-dev.in2p3.fr/CentOS7/EDEN-2.0/opt/euclid/SHE_Pipeline/params/sensitivity_isf.txt --serverurl="http://localhost:50000"
