f3ce0ca3b193e06649ed9cae626f265c5e8c8562
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-08-16 13:59:26 +0100

    Couple bugfixes

--- SHE_Pipeline/scripts/call_sensitivity_testing.sh
+++ SHE_Pipeline/scripts/call_sensitivity_testing.sh
@@ -10,9 +10,9 @@ ARCHIVE_DIR="/home/user/Work/sens_archive"
 for TAG in Ep0Pp0Sp0 Ep1Pp0Sp0 Ep2Pp0Sp0 Em1Pp0Sp0 Em2Pp0Sp0 Ep0Pp1Sp0 Ep0Pp2Sp0 Ep0Pm1Sp0 Ep0Pm2Sp0 Ep0Pp0Sp1 Ep0Pp0Sp2 Ep0Pp0Sm1 Ep0Pp0Sm2
 do
 
-CMD="E-Run SHE_Pipeline 0.4.2 SHE_Pipeline_Run --pipeline bias_measurement --isf $ISF --isf_args config_template $CFG_TEMPLATE_HEAD/Sensitivity$TAG$CFG_TEMPLATE_TAIL --workdir $WORKSPACE_ROOT$TAG --config_args SHE_CTE_MeasureBias_archive_dir $ARCHIVE_DIR"
+CMD="E-Run SHE_Pipeline 0.4.2 SHE_Pipeline_Run --pipeline bias_measurement --isf $ISF --isf_args config_template $CFG_TEMPLATE_HEAD$TAG$CFG_TEMPLATE_TAIL --workdir $WORKSPACE_ROOT$TAG --config_args SHE_CTE_MeasureBias_archive_dir $ARCHIVE_DIR"
 
 echo "Executing command: $CMD"
-exec $CMD
+eval $CMD
 
 done
