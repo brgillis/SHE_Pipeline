05dfb586724ebf0ade99fe38628fcc5e318c97c0
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-08-16 15:32:27 +0100

    Fixing directories

--- SHE_Pipeline/scripts/call_sensitivity_testing.sh
+++ SHE_Pipeline/scripts/call_sensitivity_testing.sh
@@ -1,19 +1,19 @@
 #/bin/bash
 
-ISF="AUX/SHE_Pipeline/bias_measurement_isf.txt"
+ISF="AUX/SHE_Pipeline/bias_measurement_nano_isf.txt"
 CFG_TEMPLATE_HEAD="AUX/SHE_GST_PrepareConfigs/Sensitivity"
 CFG_TEMPLATE_TAIL="Template.conf"
 
-# WORKSPACE_ROOT="/euclid/euclid-ial/workspace_"
-# ARCHIVE_DIR="/mnt/lustre/euclid-ial/sens_archive"
+ARCHIVE_DIR="/euclid/euclid-ial/sens_archive"
+WORKSPACE_ROOT="/mnt/lustre/euclid-workdir/sens_"
 
-WORKSPACE_ROOT="/home/user/Work/workspace_"
-ARCHIVE_DIR="/home/user/Work/sens_archive"
+# WORKSPACE_ROOT="/home/user/Work/workspace_"
+# ARCHIVE_DIR="/home/user/Work/sens_archive"
 
 for TAG in Ep0Pp0Sp0 Ep1Pp0Sp0 Ep2Pp0Sp0 Em1Pp0Sp0 Em2Pp0Sp0 Ep0Pp1Sp0 Ep0Pp2Sp0 Ep0Pm1Sp0 Ep0Pm2Sp0 Ep0Pp0Sp1 Ep0Pp0Sp2 Ep0Pp0Sm1 Ep0Pp0Sm2
 do
 
-CMD="E-Run SHE_Pipeline 0.4.2 SHE_Pipeline_Run --pipeline bias_measurement --isf $ISF --isf_args config_template $CFG_TEMPLATE_HEAD$TAG$CFG_TEMPLATE_TAIL --workdir $WORKSPACE_ROOT$TAG --config_args SHE_CTE_MeasureBias_archive_dir $ARCHIVE_DIR SHE_CTE_MeasureStatistics_archive_dir $ARCHIVE_DIR  --app_workdir /euclid/euclid-ial/app_workdir"
+CMD="E-Run SHE_Pipeline 0.4.2 SHE_Pipeline_Run --pipeline bias_measurement --isf $ISF --isf_args config_template $CFG_TEMPLATE_HEAD$TAG$CFG_TEMPLATE_TAIL --workdir $WORKSPACE_ROOT$TAG --config_args SHE_CTE_MeasureBias_archive_dir $ARCHIVE_DIR SHE_CTE_MeasureStatistics_archive_dir $ARCHIVE_DIR --app_workdir /euclid/euclid-ial/app_workdir"
 
 echo "Executing command: $CMD"
 eval $CMD
