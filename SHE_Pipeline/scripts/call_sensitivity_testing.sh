81af8488da141983d96e76b5d46b00c77ea6fee3
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-08-17 15:21:08 +0100

    Adding index of batch to workdir

--- SHE_Pipeline/scripts/call_sensitivity_testing.sh
+++ SHE_Pipeline/scripts/call_sensitivity_testing.sh
@@ -17,16 +17,16 @@ SEED_START=1
 
 SLEEP_TIME=20m
 
-for I in (0..4000)
+for I in {0..4000}
 do
 
 	let "SEED_MIN=SEED_START + $BATCH_SIZE*$I"
-    let "SEED_MAX=SEED_START + $BATCH_SIZE*($I+1) - 1"
+	let "SEED_MAX=SEED_START + $BATCH_SIZE*($I+1) - 1"
 
 	for TAG in Ep0Pp0Sp0 Ep1Pp0Sp0 Ep2Pp0Sp0 Em1Pp0Sp0 Em2Pp0Sp0 Ep0Pp1Sp0 Ep0Pp2Sp0 Ep0Pm1Sp0 Ep0Pm2Sp0 Ep0Pp0Sp1 Ep0Pp0Sp2 Ep0Pp0Sm1 Ep0Pp0Sm2
 	do
 
-		CMD="E-Run SHE_Pipeline 0.4.2 SHE_Pipeline_Run --pipeline bias_measurement --isf $ISF --isf_args config_template $CFG_TEMPLATE_HEAD$TAG$CFG_TEMPLATE_TAIL --workdir $WORKSPACE_ROOT$TAG --config_args SHE_CTE_MeasureBias_archive_dir $ARCHIVE_DIR SHE_CTE_MeasureStatistics_archive_dir $ARCHIVE_DIR --app_workdir /euclid/euclid-ial/app_workdir" --plan_args MSEED_MIN $SEED_MIN MSEED_MAX $SEED_MAX NSEED_MIN $SEED_MIN NSEED_MAX $SEED_MAX NUM_GALAXIES $NUM_GALAXIES
+		CMD="E-Run SHE_Pipeline 0.4.3 SHE_Pipeline_Run --pipeline bias_measurement --isf $ISF --isf_args config_template $CFG_TEMPLATE_HEAD$TAG$CFG_TEMPLATE_TAIL --workdir $WORKSPACE_ROOT"$I"_$TAG --config_args SHE_CTE_MeasureBias_archive_dir $ARCHIVE_DIR SHE_CTE_MeasureStatistics_archive_dir $ARCHIVE_DIR --app_workdir /euclid/euclid-ial/app_workdir --plan_args MSEED_MIN $SEED_MIN MSEED_MAX $SEED_MAX NSEED_MIN $SEED_MIN NSEED_MAX $SEED_MAX NUM_GALAXIES $NUM_GALAXIES"
 
 		echo "Executing command: $CMD"
 		eval $CMD
