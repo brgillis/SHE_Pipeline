1cfe4e6ab71fb780f8a5fb6529f0c2f85b29d5c2
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-08-23 15:46:09 +0200

    More fixes to status call

--- SHE_Pipeline/python/SHE_Pipeline/run_pipeline.py
+++ SHE_Pipeline/python/SHE_Pipeline/run_pipeline.py
@@ -504,7 +504,7 @@ def execute_pipeline(pipeline, isf, serverurl, workdir, wait=False):
             sleep(poll_interval)
             time_elapsed += poll_interval
             
-            status_line=sbp.run(['curl -H','"Accept: application/json"','"localhost:50000/runs/'+run_id+'"'],
+            status_line=sbp.run(['curl', '-H "Accept: application/json" "localhost:50000/runs/'+run_id+'"'],
                                 stdout=sbp.PIPE).stdout.decode('utf-8')
             logger.debug("Full status is: " + status_line)
             state = dict(status_line)["executionStatus"]
