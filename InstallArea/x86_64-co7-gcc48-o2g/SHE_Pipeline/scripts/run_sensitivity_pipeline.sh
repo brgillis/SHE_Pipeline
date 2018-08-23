50aabc61a4e9cb0e18a44505728963451bc968ff
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-08-23 15:08:41 +0200

    Removing InstallArea from repo

--- InstallArea/x86_64-co7-gcc48-o2g/SHE_Pipeline/scripts/run_sensitivity_pipeline.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-# echo password | sudo -S systemctl restart euclid-ial-wfm
-# sleep 1
-
-pipeline_runner.py --configure --config=/cvmfs/euclid-dev.in2p3.fr/CentOS7/EDEN-2.0/opt/euclid/SHE_Pipeline/0.3/InstallArea/x86_64-co7-gcc48-o2g/auxdir/SHE_Pipeline/euclid_prs_app.cfg --serverurl="http://localhost:50000"
-pipeline_runner.py --pipeline=sensitivity_pipeline.py --data=/cvmfs/euclid-dev.in2p3.fr/CentOS7/EDEN-2.0/opt/euclid/SHE_Pipeline/0.3/InstallArea/x86_64-co7-gcc48-o2g/auxdir/SHE_Pipeline/sensitivity_isf.txt --serverurl="http://localhost:50000"
