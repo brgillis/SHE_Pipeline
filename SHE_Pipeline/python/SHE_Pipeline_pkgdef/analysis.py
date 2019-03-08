24f9b076c3178733f02db1a0ce03037d6201748f
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-03-08 16:29:44 +0000

    Updating Analysis pipeline by copying from SHE_IAL_Pipelines

--- SHE_Pipeline/python/SHE_Pipeline_pkgdef/analysis.py
+++ SHE_Pipeline/python/SHE_Pipeline_pkgdef/analysis.py
@@ -5,7 +5,7 @@
     Pipeline script for the shear-estimation-only pipeline.
 """
 
-__updated__ = "2019-01-25"
+__updated__ = "2019-03-08"
 
 # Copyright (C) 2012-2020 Euclid Science Ground Segment
 #
@@ -20,7 +20,7 @@ __updated__ = "2019-01-25"
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-from SHE_Pipeline_pkgdef.package_definition import she_fit_psf, she_estimate_shear, she_validate_shear
+from SHE_Pipeline_pkgdef.analysis_pkgdef import she_fit_psf, she_estimate_shear, she_validate_shear
 from euclidwf.framework.workflow_dsl import pipeline
 
 
@@ -34,50 +34,59 @@ def she_remap_mosaics(mer_tile_listfile, vis_prod_filenames):
 
 
 @pipeline(outputs=('validated_shear_estimates_table'))
-def shear_analysis_pipeline(data_images,
-                            stacked_image,
-                            mer_tiles,
-                            detections_tables,
+def shear_analysis_pipeline(mdb,
+                            vis_image,
+                            vis_stacked_image,
+                            mer_segmentation_map,
+                            mer_catalog,
                             # aocs_time_series_products, # Disabled for now
                             # psf_calibration_products, # Disabled for now
                             # galaxy_population_priors_table, # Disabled for now
                             # calibration_parameters_product, # Disabled for now
                             phz_output_cat,
                             spe_output_cat,
+                            ksb_training_data,
+                            lensmc_training_data,
+                            regauss_training_data,
+                            bfd_training_data,
+                            momentsml_training_data,
                             ):
 
-    stacked_segmentation_image = she_remap_mosaic(mer_tile_listfile=mer_tiles,
-                                                  vis_prod_filename=stacked_image,)
+    stacked_segmentation_image = she_remap_mosaic(mer_tile_listfile=mer_segmentation_map,
+                                                  vis_prod_filename=vis_stacked_image,)
 
-    segmentation_images = she_remap_mosaics(mer_tile_listfile=mer_tiles,
-                                            vis_prod_filenames=data_images)
+    segmentation_images = she_remap_mosaics(mer_tile_listfile=mer_segmentation_map,
+                                            vis_prod_filenames=vis_image)
 
-    psf_field_params = she_fit_psf(data_images=data_images,
-                                   segmentation_images=segmentation_images,
-                                   detections_tables=detections_tables,
+    psf_field_params = she_fit_psf(data_images=vis_image,
+                                   segmentation_images=mer_segmentation_map,
+                                   detections_tables=mer_catalog,
+                                   mdb=mdb,
                                    # aocs_time_series_products = aocs_time_series_products, # Disabled for now
                                    # psf_calibration_products = psf_calibration_products, # Disabled for now
                                    )
 
-    psf_images_and_tables = she_model_psf(data_images=data_images,
-                                          segmentation_images=segmentation_images,
-                                          detections_tables=detections_tables,
+    psf_images_and_tables = she_model_psf(data_images=vis_image,
+                                          segmentation_images=mer_segmentation_map,
+                                          detections_tables=mer_catalog,
+                                          mdb=mdb,
                                           # aocs_time_series_products = aocs_time_series_products, # Disabled for now
                                           # psf_calibration_products = psf_calibration_products, # Disabled for now
                                           psf_field_params=psf_field_params,
                                           )
 
-    shear_estimates_product = she_estimate_shear(data_images=data_images,
-                                                 stacked_image=stacked_image,
+    shear_estimates_product = she_estimate_shear(data_images=vis_image,
+                                                 stacked_image=vis_stacked_image,
                                                  psf_images_and_tables=psf_images_and_tables,
-                                                 segmentation_images=segmentation_images,
+                                                 segmentation_images=mer_segmentation_map,
                                                  stacked_segmentation_image=stacked_segmentation_image,
-                                                 detections_tables=detections_tables,
-                                                 # bfd_training_data = bfd_training_data, # Disabled for now
-                                                 # ksb_training_data = ksb_training_data, # Disabled for now
-                                                 # lensmc_training_data = lensmc_training_data, # Disabled for now
-                                                 # momentsml_training_data = momentsml_training_data, # Disabled for now
-                                                 # regauss_training_data = regauss_training_data, # Disabled for now
+                                                 detections_tables=mer_catalog,
+                                                 bfd_training_data=bfd_training_data,
+                                                 ksb_training_data=ksb_training_data,
+                                                 lensmc_training_data=lensmc_training_data,
+                                                 momentsml_training_data=momentsml_training_data,
+                                                 regauss_training_data=regauss_training_data,
+                                                 mdb=mdb,
                                                  # galaxy_population_priors_table=galaxy_population_priors_table,
                                                  # calibration_parameters_product = calibration_parameters_product, #
                                                  # Disabled for now
---
+++ SHE_Pipeline/python/SHE_Pipeline_pkgdef/analysis_pkgdef.py
@@ -0,0 +1,75 @@
+""" @file analysis_pkgdef.py
+
+    Created 29 Aug 2017
+
+    Package definition for the OU-SHE analysis pipeline.
+"""
+
+__updated__ = "2019-03-08"
+
+# Copyright (C) 2012-2020 Euclid Science Ground Segment
+#
+# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+from euclidwf.framework.taskdefs import Executable, Input, Output, ComputingResources
+
+ERun_CTE = "E-Run SHE_CTE 0.7 "
+ERun_MER = "E-Run SHE_MER 0.3 "
+
+she_remap_mosaic = Executable(command=ERun_MER + "SHE_MER_RemapMosaic",
+                              inputs=[Input("mer_tile_listfile", content_type="listfile"),
+                                      Input("vis_prod_filename")],
+                              outputs=[Output("output_filename", mime_type='xml')])
+
+she_fit_psf = Executable(command=ERun_CTE + "SHE_CTE_FitPSFs",
+                         inputs=[Input("data_images", content_type="listfile"),
+                                 Input("detections_tables", content_type="listfile"),
+                                 Input("segmentation_images", content_type="listfile"),
+                                 Input("mdb"),
+                                 # Input("aocs_time_series_products", content_type="listfile"), # Disabled for now
+                                 # Input("psf_calibration_products", content_type="listfile"), # Disabled for now
+                                 ],
+                         outputs=[Output("psf_field_params", mime_type="json", content_type="listfile")])
+
+she_model_psf = Executable(command=ERun_CTE + "SHE_CTE_ModelPSFs",
+                           inputs=[Input("data_images", content_type="listfile"),
+                                   Input("detections_tables", content_type="listfile"),
+                                   Input("segmentation_images", content_type="listfile"),
+                                   Input("psf_field_params"),
+                                   Input("mdb"),
+                                   # Input("aocs_time_series_products", content_type="listfile"), # Disabled for now
+                                   # Input("psf_calibration_products", content_type="listfile"), # Disabled for now
+                                   ],
+                           outputs=[Output("psf_images_and_tables", mime_type="json", content_type="listfile")])
+
+she_estimate_shear = Executable(command=ERun_CTE + "SHE_CTE_EstimateShear",
+                                inputs=[Input("data_images", content_type="listfile"),
+                                        Input("stacked_image"),
+                                        Input("psf_images_and_tables", content_type="listfile"),
+                                        Input("segmentation_images", content_type="listfile"),
+                                        Input("stacked_segmentation_image"),
+                                        Input("detections_tables", content_type="listfile"),
+                                        Input("bfd_training_data"),
+                                        Input("ksb_training_data"),
+                                        Input("lensmc_training_data"),
+                                        Input("momentsml_training_data"),
+                                        Input("regauss_training_data"),
+                                        Input("pipeline_config"),
+                                        Input("mdb"),
+                                        # Input("galaxy_population_priors_table"), # Disabled for now
+                                        # Input("calibration_parameters_product"), # Disabled for now
+                                        ],
+                                outputs=[Output("shear_estimates_product", mime_type="xml"), ])
+
+she_cross_validate_shear = Executable(command=ERun_CTE + "SHE_CTE_CrossValidateShear",
+                                      inputs=[Input("shear_estimates_product")],
+                                      outputs=[Output("cross_validated_shear_estimates_product", mime_type="xml")])
