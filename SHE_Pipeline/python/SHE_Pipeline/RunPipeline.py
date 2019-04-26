2b93bcf2641f41b1cfee47445f705e146eea51d2
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-04-26 15:59:31 +0100

    Fixing store_true cline-args

--- SHE_Pipeline/python/SHE_Pipeline/RunPipeline.py
+++ SHE_Pipeline/python/SHE_Pipeline/RunPipeline.py
@@ -5,7 +5,7 @@
     Main program for calling one of the pipelines.
 """
 
-__updated__ = "2019-04-24"
+__updated__ = "2019-04-26"
 
 # Copyright (C) 2012-2020 Euclid Science Ground Segment
 #
@@ -22,8 +22,9 @@ __updated__ = "2019-04-24"
 
 import argparse
 
-from ElementsKernel.Logging import getLogger
 from SHE_PPT.utility import get_arguments_string
+
+from ElementsKernel.Logging import getLogger
 import SHE_Pipeline
 from SHE_Pipeline.run_pipeline import run_pipeline_from_args
 
@@ -94,8 +95,8 @@ def mainMethod(args):
     logger.debug('# Entering SHE_Pipeline_Run mainMethod()')
     logger.debug('#')
 
-    exec_cmd = get_arguments_string(args, cmd="E-Run SHE_Pipeline "+SHE_Pipeline.__version__+" SHE_Pipeline_Run",
-                                    store_true=["profile", "debug", "cluster", "wait", "no_local_wait"])
+    exec_cmd = get_arguments_string(args, cmd="E-Run SHE_Pipeline " + SHE_Pipeline.__version__ + " SHE_Pipeline_Run",
+                                    store_true=["profile", "debug", "cluster", "use_debug_server_config"])
     logger.info('Execution command for this step:')
     logger.info(exec_cmd)
 
