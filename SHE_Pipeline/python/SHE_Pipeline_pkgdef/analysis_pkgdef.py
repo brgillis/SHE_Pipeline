1873dc23bae358930d03acc42580142cac58200f
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-05-28 17:10:30 +0100

    Increasing ram for PSF Modelling

--- SHE_Pipeline/python/SHE_Pipeline_pkgdef/analysis_pkgdef.py
+++ SHE_Pipeline/python/SHE_Pipeline_pkgdef/analysis_pkgdef.py
@@ -64,7 +64,7 @@ she_model_psf = Executable(command=ERun_PSF + "SHE_PSM_ModelPSFs",
                                    # Input("psf_calibration_products", content_type="listfile"), # Disabled for now
                                    ],
                            outputs=[Output("psf_images_and_tables", mime_type="json", content_type="listfile")],
-                           resources=ComputingResources(cores=1, ram=1.9, walltime=4.0))
+                           resources=ComputingResources(cores=1, ram=3.9, walltime=4.0))
 
 she_estimate_shear = Executable(command=ERun_CTE + "SHE_CTE_EstimateShear",
                                 inputs=[Input("data_images", content_type="listfile"),
