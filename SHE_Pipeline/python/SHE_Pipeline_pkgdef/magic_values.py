2bbecb70504c67cf3bc1d9856b40ef7c55c95cc1
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-05-29 16:20:35 +0200

    Updating version of SHE_GST it depends on

--- SHE_Pipeline/python/SHE_Pipeline_pkgdef/magic_values.py
+++ SHE_Pipeline/python/SHE_Pipeline_pkgdef/magic_values.py
@@ -5,7 +5,7 @@
     Magic values for the package definitions
 """
 
-__updated__ = "2019-05-28"
+__updated__ = "2019-05-29"
 
 # Copyright (C) 2012-2020 Euclid Science Ground Segment
 #
@@ -21,7 +21,7 @@ __updated__ = "2019-05-28"
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 SHE_CTE_version = "0.8.4"
-SHE_GST_version = "1.8.2"
+SHE_GST_version = "1.8.3"
 SHE_MER_version = "0.4.3"
 SHE_Pipeline_version = "0.8.4"
 SHE_PSF_version = "2.2.1"
