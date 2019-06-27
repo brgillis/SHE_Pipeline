120c24ad1771370e11e637e7a77caf65bef5cfe4
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-06-27 16:56:40 +0200

    Incrementing dependency versions

--- SHE_Pipeline/python/SHE_Pipeline/__init__.py
+++ SHE_Pipeline/python/SHE_Pipeline/__init__.py
@@ -5,7 +5,7 @@
     Generic __init__.py
 """
 
-__updated__ = "2019-06-02"
+__updated__ = "2019-06-27"
 
 # Copyright (C) 2012-2020 Euclid Science Ground Segment
 #
@@ -20,4 +20,4 @@ __updated__ = "2019-06-02"
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-__version__ = "0.8.7"
+__version__ = "0.8.8"
