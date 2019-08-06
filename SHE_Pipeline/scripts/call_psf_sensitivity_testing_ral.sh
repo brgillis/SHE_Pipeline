d7e9d1e7865527720635b3036d5683042d202b4e
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-08-06 15:05:49 +0100

    Adding updated calling scripts

---
+++ SHE_Pipeline/scripts/call_psf_sensitivity_testing_ral.sh
@@ -0,0 +1,69 @@
+#/bin/bash
+
+QUEUE="ral"
+
+ARCHIVE_DIR="/ceph/ral/sens_psf_archive"
+WORKSPACE_ROOT="/tmp/sens_"
+LOGDIR="/ceph/ral/sens_psf_logs"
+SCRIPTDIR="/ceph/home/hpcgill1/sens_testing_scripts"
+
+SLEEP_TIME=1m
+RETRY_SLEEP=5m
+
+JOB_LIMIT=200
+
+SEED_START=1
+SEEDS_PER_BATCH=104
+NUM_GALAXIES_PER_SEED=16
+
+NUM_BATCHES=50
+NUM_THREADS=8
+
+KILLFILE="DELETE_ME_TO_STOP_RAL_PSF_SCRIPT"
+
+touch $KILLFILE
+
+ALL_DONE=0
+
+while [ $ALL_DONE -eq 0 ]
+do
+
+	ALL_DONE=1
+
+	for ((I=1; I<=$NUM_BATCHES; I++))
+	do
+
+		for TAG in Pm2 Pm1 Pp0 Pp1 Pp2 Lm2 Lm1 Lp1 Lp2 Sm2 Sm1 Sp1 Sp2 Um2 Um1 Up1 Up2 Rm2 Rm1 Rp1 Rp2 Xm2 Xm1 Xp1 Xp2
+		do
+
+			if [ ! -f $KILLFILE ]; then
+			    exit 0
+			fi
+
+			if [ -f $ARCHIVE_DIR/$TAG/sens_psf_$I/sens_$I\_$TAG/data/shear_bias_measurements_final.xml ]; then
+			    continue
+			fi
+			ALL_DONE=0
+
+			mkdir -p $ARCHIVE_DIR/$TAG
+
+			CMD="sbatch -p $QUEUE -N 1 -n $NUM_THREADS -o $LOGDIR/sens_testing_"$TAG"_"$I".out -e $LOGDIR/sens_testing_"$TAG"_"$I".err $SCRIPTDIR/run_bias_measurement_pipeline.sh $SEED_START $SEEDS_PER_BATCH $NUM_GALAXIES_PER_SEED $TAG $I $NUM_THREADS $ARCHIVE_DIR $WORKSPACE_ROOT $SCRIPTDIR"
+
+			RUNNING_JOBS=`squeue | grep $QUEUE | wc -l`
+			while [ $RUNNING_JOBS -ge $JOB_LIMIT ]
+			do
+			    sleep $SLEEP_TIME
+			    RUNNING_JOBS=`squeue | grep $QUEUE | wc -l`
+			done
+
+			echo "Executing command: $CMD"
+			eval $CMD
+
+		done
+
+	done
+
+	sleep $RETRY_SLEEP
+
+done 
+
