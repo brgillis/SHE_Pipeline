7f99910a1a37d7a28b1314de07e053454c9e3fcf
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-08-16 17:59:32 +0100

    Fixing version to 0.8.13

--- SHE_Pipeline/python/SHE_Pipeline_pkgdef/__init__.py
+++ SHE_Pipeline/python/SHE_Pipeline_pkgdef/__init__.py
@@ -5,7 +5,7 @@
     Generic __init__.py
 """
 
-__updated__ = "2019-07-26"
+__updated__ = "2019-08-16"
 
 # Copyright (C) 2012-2020 Euclid Science Ground Segment
 #
@@ -20,4 +20,4 @@ __updated__ = "2019-07-26"
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-__version__ = "0.8.11"
+__version__ = "0.8.13"
