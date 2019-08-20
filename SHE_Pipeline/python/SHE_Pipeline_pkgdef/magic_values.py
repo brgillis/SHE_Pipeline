a6cbb9d1d9daa49bcaf14055c2f1a50844256033
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-08-20 18:02:19 +0200

    Incrementing to version 0.8.15

--- SHE_Pipeline/python/SHE_Pipeline_pkgdef/magic_values.py
+++ SHE_Pipeline/python/SHE_Pipeline_pkgdef/magic_values.py
@@ -5,7 +5,7 @@
     Magic values for the package definitions
 """
 
-__updated__ = "2019-08-19"
+__updated__ = "2019-08-20"
 
 # Copyright (C) 2012-2020 Euclid Science Ground Segment
 #
@@ -23,7 +23,7 @@ __updated__ = "2019-08-19"
 SHE_CTE_version = "0.8.11"
 SHE_GST_version = "1.8.6"
 SHE_MER_version = "0.4.8"
-SHE_Pipeline_version = "0.8.14"
+SHE_Pipeline_version = "0.8.15"
 SHE_PSF_version = "2.4.1"
 
 ERun_CTE = "E-Run SHE_CTE " + SHE_CTE_version + " "
