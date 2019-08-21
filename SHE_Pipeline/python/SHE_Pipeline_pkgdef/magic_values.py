4ff918ba76b0ba77afda53f7da6e79a5974c9354
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-08-21 14:30:09 +0100

    Get version of projects from their __version__ variables

--- SHE_Pipeline/python/SHE_Pipeline_pkgdef/magic_values.py
+++ SHE_Pipeline/python/SHE_Pipeline_pkgdef/magic_values.py
@@ -5,7 +5,7 @@
     Magic values for the package definitions
 """
 
-__updated__ = "2019-07-23"
+__updated__ = "2019-08-21"
 
 # Copyright (C) 2012-2020 Euclid Science Ground Segment
 #
@@ -20,14 +20,14 @@ __updated__ = "2019-07-23"
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-SHE_CTE_version = "0.7"
-SHE_GST_version = "1.7"
-SHE_MER_version = "0.3"
-SHE_Pipeline_version = "0.7"
-SHE_PSF_version = "2.3"
+import SHE_CTE
+import SHE_GST
+import SHE_MER_RemapMosaic
+import SHE_PSFToolkit
+import SHE_Pipeline
 
-ERun_CTE = "E-Run SHE_CTE " + SHE_CTE_version + " "
-ERun_MER = "E-Run SHE_MER " + SHE_MER_version + " "
-ERun_GST = "E-Run SHE_GST " + SHE_GST_version + " "
-ERun_PSF = "E-Run SHE_PSFToolkit " + SHE_PSF_version + " "
-ERun_Pipeline = "E-Run SHE_Pipeline " + SHE_Pipeline_version + " "
+ERun_CTE = "E-Run SHE_CTE " + SHE_CTE.__version__ + " "
+ERun_MER = "E-Run SHE_MER " + SHE_MER_RemapMosaic.__version__ + " "
+ERun_GST = "E-Run SHE_GST " + SHE_GST.__version__ + " "
+ERun_PSF = "E-Run SHE_PSFToolkit " + SHE_PSFToolkit.__version__ + " "
+ERun_Pipeline = "E-Run SHE_Pipeline " + SHE_Pipeline.__version__ + " "
