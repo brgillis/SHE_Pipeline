79f1831b45f56a256826a4708ff7e9a529646ff4
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-08-27 17:34:49 +0200

    Incrementing to version 0.8.17 and updating dependencies

--- SHE_Pipeline/python/SHE_Pipeline_pkgdef/magic_values.py
+++ SHE_Pipeline/python/SHE_Pipeline_pkgdef/magic_values.py
@@ -5,7 +5,7 @@
     Magic values for the package definitions
 """
 
-__updated__ = "2019-08-21"
+__updated__ = "2019-08-27"
 
 # Copyright (C) 2012-2020 Euclid Science Ground Segment
 #
@@ -20,11 +20,11 @@ __updated__ = "2019-08-21"
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-SHE_CTE_version = "0.8.11"
-SHE_GST_version = "1.8.6"
-SHE_MER_version = "0.4.8"
-SHE_Pipeline_version = "0.8.16"
-SHE_PSF_version = "2.4.1"
+SHE_CTE_version = "0.8.12"
+SHE_GST_version = "1.8.7"
+SHE_MER_version = "0.4.9"
+SHE_Pipeline_version = "0.8.17"
+SHE_PSF_version = "2.4.2"
 
 ERun_CTE = "E-Run SHE_CTE " + SHE_CTE_version + " "
 ERun_MER = "E-Run SHE_MER " + SHE_MER_version + " "
