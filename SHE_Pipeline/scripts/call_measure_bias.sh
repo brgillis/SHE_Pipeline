15efb38e5a883fd03847f0ba3c5752b0fc2f113d
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-10-17 15:40:47 +0200

    Updating scripts to measure bias

--- SHE_Pipeline/scripts/call_measure_bias.sh
+++ SHE_Pipeline/scripts/call_measure_bias.sh
@@ -1,11 +1,23 @@
 #/bin/bash
 
-ARCHIVE_DIR="/euclid/euclid-ial/sens_archive"
+QUEUE="ral"
+DATA_DIR="/data"
+ARCHIVE_DIR=$DATA_DIR"/sens_archive"
+LOGDIR=$DATA_DIR"/sens_logs"
+SCRIPTDIR="/ceph/home/hpcgill1/sens_testing_scripts"
+
+NUM_THREADS=8
+
+mkdir -p $LOGDIR
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 
 for TAG in Ep0Pp0Sp0 Ep1Pp0Sp0 Ep2Pp0Sp0 Em1Pp0Sp0 Em2Pp0Sp0 Ep0Pp1Sp0 Ep0Pp2Sp0 Ep0Pm1Sp0 Ep0Pm2Sp0 Ep0Pp0Sp1 Ep0Pp0Sp2 Ep0Pp0Sm1 Ep0Pp0Sm2
 do
 
-    CMD="E-Run SHE_CTE 0.6.7 SHE_CTE_MeasureBias --workdir $ARCHIVE_DIR/$TAG --archive_dir $ARCHIVE_DIR/$TAG --bootstrap_seed 1 --shear_bias_measurements shear_bias_measurements.xml"
+    CMD="sbatch -p $QUEUE -N 1 -n $NUM_THREADS -o $LOGDIR/sens_testing_"$TAG"_combine.out -e $LOGDIR/sens_testing_"$TAG"_combine.err $SCRIPTDIR/run_bias_measurement_pipeline.sh $ARCHIVE_DIR $TAG"
 
     echo "Executing command: $CMD"
     eval $CMD
