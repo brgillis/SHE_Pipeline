bb398d41162c20ea222bf66a60ec5cbd6a00a761
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-10-28 18:17:50 +0000

    Incrementing to v0.8.24, depends on SHE_CTE 0.8.17

--- SHE_Pipeline/python/SHE_Pipeline_pkgdef/magic_values.py
+++ SHE_Pipeline/python/SHE_Pipeline_pkgdef/magic_values.py
@@ -20,10 +20,10 @@ __updated__ = "2019-10-28"
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-SHE_CTE_version = "0.8.16"
+SHE_CTE_version = "0.8.17"
 SHE_GST_version = "1.8.9"
 SHE_MER_version = "0.4.9"
-SHE_Pipeline_version = "0.8.23"
+SHE_Pipeline_version = "0.8.24"
 SHE_PSF_version = "2.4.2"
 
 ERun_CTE = "E-Run SHE_CTE " + SHE_CTE_version + " "
