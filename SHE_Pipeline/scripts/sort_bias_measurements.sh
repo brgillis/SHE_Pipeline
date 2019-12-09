f756bc4123867f495e721064c84365e7da6b5da0
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-12-09 15:57:11 +0000

    Tarring up resulting shear bias measurements

--- SHE_Pipeline/scripts/sort_bias_measurements.sh
+++ SHE_Pipeline/scripts/sort_bias_measurements.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+TAR_STRING="tar cvzf shear_bias_measurements.tar.gz "
+
 for TAG in Ep0Pp0Sp0 Em2Pp0Sp0 Em1Pp0Sp0 Ep1Pp0Sp0 Ep2Pp0Sp0 Ep0Pp0Sm2 Ep0Pp0Sm1 Ep0Pp0Sp1 Ep0Pp0Sp2
 do
   NEW_TAG=${TAG}Tp0
   cp $TAG/shear_bias_measurements.xml shear_bias_measurements_${NEW_TAG}.xml
   cp -r $TAG/data .
+  TAR_STRING=$TAR_STRING"shear_bias_measurements_"$NEW_TAG".xml "
 done
 
 for TAG in Ep0Pm2Sp0 Ep0Pm1Sp0 Ep0Pp1Sp0 Ep0Pp2Sp0
 do
   NEW_TAG=${TAG}Tp0
   cp Ep0Pp0Sp0/shear_bias_measurements.xml shear_bias_measurements_${NEW_TAG}.xml
+  TAR_STRING=$TAR_STRING"shear_bias_measurements_"$NEW_TAG".xml "
 done
 
 for TAG in Tm2 Tm1 Tp1 Tp2
@@ -18,6 +22,7 @@ do
   NEW_TAG=Ep0Pp0Sp0${TAG}
   cp $TAG/shear_bias_measurements.xml shear_bias_measurements_${NEW_TAG}.xml
   cp -r $TAG/data .
+  TAR_STRING=$TAR_STRING"shear_bias_measurements_"$NEW_TAG".xml "
 done
 
 for TAG in CO WB COWB
@@ -25,4 +30,7 @@ do
   NEW_TAG=$TAG
   cp $TAG/shear_bias_measurements.xml shear_bias_measurements_${!NEW_TAG}.xml
   cp -r $TAG/data .
+  TAR_STRING=$TAR_STRING"shear_bias_measurements_"$NEW_TAG".xml "
 done
+
+eval $TAR_STRING
