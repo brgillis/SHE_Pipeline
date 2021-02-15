667338a88968f4d576b1765cdc9bbda2cd457ce2
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2021-02-15 12:21:03 +0100

    Updating default template workdir

--- SHE_Pipeline/scripts/clone_workdir.sh
+++ SHE_Pipeline/scripts/clone_workdir.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SOURCEDIR=/mnt/cephfs/share/SC7_migrated/SC7M_workdir
+SOURCEDIR=/mnt/cephfs/share/SC7_migrated/SC7M_805_workdir
 
 # Check we're given one argument
 if [ "$#" -eq 1 ]; then
