fb62d58e8422dbf59e7ca4bc7d25615db0c9c8b0
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2021-07-22 11:40:04 +0100

    Fixing values from variables

--- SHE_Pipeline/scripts/create_val_listfiles.sh
+++ SHE_Pipeline/scripts/create_val_listfiles.sh
@@ -3,8 +3,8 @@
 SHE_JSON=she_measurements_listfile.json
 SIM_JSON=tu_output_listfile.json
 
-echo -n [ >> SHE_JSON
-echo -n [ >> SIM_JSON
+echo -n [ >> $SHE_JSON
+echo -n [ >> $SIM_JSON
 
 for i in {0..100000}
 do
@@ -14,15 +14,15 @@ do
     fi
 	SIM_FN=`grep -l \<EuclidPointingId\>$i\</EuclidPointingId\> *.xml`
 
-	echo -n \" >> SHE_JSON
-	echo -n \" >> SIM_JSON
+	echo -n \" >> $SHE_JSON
+	echo -n \" >> $SIM_JSON
 	
-	echo -n $SHE_FN >> SHE_JSON
-	echo -n $SIM_FN >> SIM_JSON	
+	echo -n $SHE_FN >> $SHE_JSON
+	echo -n $SIM_FN >> $SIM_JSON	
 
-	echo -n \", >> SHE_JSON
-	echo -n \", >> SIM_JSON
+	echo -n \", >> $SHE_JSON
+	echo -n \", >> $SIM_JSON
 done
 
-echo -n ] >> SHE_JSON
-echo -n ] >> SIM_JSON
+echo -n ] >> $SHE_JSON
+echo -n ] >> $SIM_JSON
