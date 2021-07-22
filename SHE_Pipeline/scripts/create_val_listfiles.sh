a7b39cfb39db52093975e8315103b763dad3a421
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2021-07-22 11:10:27 +0100

    Writing script to create validation listfiles

---
+++ SHE_Pipeline/scripts/create_val_listfiles.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+SHE_JSON = "she_measurements_listfile.json"
+SIM_JSON = "tu_output_listfile.json"
+
+echo -n [ >> SHE_JSON
+echo -n [ >> SIM_JSON
+
+for i in {0..100000}
+do
+	SHE_FN=`grep -l \<ObservationId\>$i\</ObservationId\> *.xml`
+	if [ $? -eq 1 ]; then
+		continue
+    fi
+	SIM_FN=`grep -l \<EuclidPointingId\>$i\</EuclidPointingId\> *.xml`
+
+	echo -n \" >> SHE_JSON
+	echo -n \" >> SIM_JSON
+	
+	echo -n $SHE_FN >> SHE_JSON
+	echo -n $SIM_FN >> SIM_JSON	
+
+	echo -n \", >> SHE_JSON
+	echo -n \", >> SIM_JSON
+done
+
+echo -n ] >> SHE_JSON
+echo -n ] >> SIM_JSON
