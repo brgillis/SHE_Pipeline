a6cb205743f5ab3143d76ca889e13399106c1812
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2021-09-20 16:22:23 +0100

    Adding some functionality to getting mer products

--- SHE_Pipeline/scripts/get_all_mer_products.sh
+++ SHE_Pipeline/scripts/get_all_mer_products.sh
@@ -1,9 +1,17 @@
 #/bin/bash
 
 DATASETRELEASE=MER_GSIR_SWF1_R3_V1
+# OBS_ID=10351
 
-# Get the DpdVisStackedFrame product and fits files
-python $HOME/bin/dataProductRetrieval_SC8.py --username `cat $HOME/.username.txt` --password `cat $HOME/.password.txt` --project TEST --data_product DpdMerFinalCatalog --query "Header.DataSetRelease=$DATASETRELEASE"
+QUERY="Header.ManualValidationStatus!=INVALID&&Header.DataSetRelease=$DATASETRELEASE"
 
-# Get the DpdVisCalibratedFrame product and fits files
-python $HOME/bin/dataProductRetrieval_SC8.py --username `cat $HOME/.username.txt` --password `cat $HOME/.password.txt` --project TEST --data_product DpdMerSegmentationMap --query "Header.DataSetRelease=$DATASETRELEASE"
+if [ -v ${OBS_ID+x} ]
+then
+  QUERY=$QUERY"&&Data.ObservationIdList==$OBS_ID"
+fi
+
+# Get the DpdMerFinalCatalog product and fits files
+python $HOME/bin/dataProductRetrieval_SC8.py --username `cat $HOME/.username.txt` --password `cat $HOME/.password.txt` --project TEST --data_product DpdMerFinalCatalog --query $QUERY
+
+# Get the DpdMerSegmentationMap product and fits files
+python $HOME/bin/dataProductRetrieval_SC8.py --username `cat $HOME/.username.txt` --password `cat $HOME/.password.txt` --project TEST --data_product DpdMerSegmentationMap --query $QUERY
