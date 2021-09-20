1916d4d8d1ecf9a049e7e638f54bab1c7187baf0
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2021-09-20 16:51:42 +0100

    Fixing some bugs in script

--- SHE_Pipeline/scripts/get_all_mer_products.sh
+++ SHE_Pipeline/scripts/get_all_mer_products.sh
@@ -1,11 +1,12 @@
 #/bin/bash
 
-DATASETRELEASE=MER_GSIR_SWF1_R3_V1
+DATASETRELEASE=SC8_MAIN_V0
 # OBS_ID=10351
 
-QUERY="Header.ManualValidationStatus!=INVALID&&Header.DataSetRelease=$DATASETRELEASE"
+QUERY="Header.ManualValidationStatus.ManualValidationStatus!=\"INVALID\"&&Header.DataSetRelease=$DATASETRELEASE"
+# QUERY="Header.DataSetRelease=$DATASETRELEASE"
 
-if [ -v ${OBS_ID+x} ]
+if [ ! -z ${OBS_ID+x} ]
 then
   QUERY=$QUERY"&&Data.ObservationIdList==$OBS_ID"
 fi
@@ -13,7 +14,11 @@ fi
 echo "Query: $QUERY"
 
 # Get the DpdMerFinalCatalog product and fits files
-python $HOME/bin/dataProductRetrieval_SC8.py --username `cat $HOME/.username.txt` --password `cat $HOME/.password.txt` --project TEST --data_product DpdMerFinalCatalog --query $QUERY
+CMD='python '$HOME'/bin/dataProductRetrieval_SC8.py --username '`cat $HOME/.username.txt`' --password '`cat $HOME/.password.txt`' --project TEST --data_product DpdMerFinalCatalog --query "'$QUERY'"'
+echo "Command: $CMD"
+eval $CMD
 
 # Get the DpdMerSegmentationMap product and fits files
-python $HOME/bin/dataProductRetrieval_SC8.py --username `cat $HOME/.username.txt` --password `cat $HOME/.password.txt` --project TEST --data_product DpdMerSegmentationMap --query $QUERY
+CMD='python '$HOME'/bin/dataProductRetrieval_SC8.py --username '`cat $HOME/.username.txt`' --password '`cat $HOME/.password.txt`' --project TEST --data_product DpdMerSegmentationMap --query "'$QUERY'"'
+echo "Command: $CMD"
+eval $CMD
