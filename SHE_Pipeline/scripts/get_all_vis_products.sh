4f7d72aeb56227e5af54bfdd406625d4fd2d9852
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2021-09-22 15:19:40 +0000

    Update SHE_Pipeline/scripts/get_all_vis_products.sh

--- SHE_Pipeline/scripts/get_all_vis_products.sh
+++ SHE_Pipeline/scripts/get_all_vis_products.sh
@@ -15,20 +15,22 @@ BASEDIR=$(dirname $(realpath "$0"))
 for PICKED_OBS_ID in $OBS_ID; do
 
   if [ $OBS_ID != all ]; then
-    QUERY_FOR_OBS=$QUERY"&&Data.ObservationSequence.ObservationId==$OBS_ID"
+    QUERY_FOR_STACK=$QUERY"&&Data.ObservationId==$OBS_ID"
+    QUERY_FOR_CALIB=$QUERY"&&Data.ObservationSequence.ObservationId==$OBS_ID"
   else
-    QUERY_FOR_OBS=$QUERY
+    QUERY_FOR_STACK=$QUERY
+    QUERY_FOR_CALIB=$QUERY
   fi
 
   echo "Query: $QUERY_FOR_OBS"
 
   # Get the DpdVisStackedFrame product and fits files
-  CMD='python '$BASEDIR'/dataProductRetrieval_SC8.py --username '`cat $HOME/.username.txt`' --password '`cat $HOME/.password.txt`' --project TEST --data_product DpdVisStackedFrame --query "'$QUERY'"'
+  CMD='python '$BASEDIR'/dataProductRetrieval_SC8.py --username '`cat $HOME/.username.txt`' --password '`cat $HOME/.password.txt`' --project TEST --data_product DpdVisStackedFrame --query "'$QUERY_FOR_STACK'"'
   echo "Command: $CMD"
   eval $CMD
 
   # Get the DpdVisCalibratedFrame product and fits files
-  CMD='python '$BASEDIR'/dataProductRetrieval_SC8.py --username '`cat $HOME/.username.txt`' --password '`cat $HOME/.password.txt`' --project TEST --data_product DpdVisCalibratedFrame --query "'$QUERY'"'
+  CMD='python '$BASEDIR'/dataProductRetrieval_SC8.py --username '`cat $HOME/.username.txt`' --password '`cat $HOME/.password.txt`' --project TEST --data_product DpdVisCalibratedFrame --query "'$QUERY_FOR_CALIB'"'
   echo "Command: $CMD"
   eval $CMD
 
