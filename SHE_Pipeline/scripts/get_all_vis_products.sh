cb9cd26e9b3ad7015b7918bda62e0e69308110b5
Giuseppe Congedo <giuseppe.congedo@ed.ac.uk>
2021-09-23 09:49:19 +0100

    fix picking right observation ID; print correct query to screen

--- SHE_Pipeline/scripts/get_all_vis_products.sh
+++ SHE_Pipeline/scripts/get_all_vis_products.sh
@@ -15,19 +15,21 @@ BASEDIR=$(dirname $(realpath "$0"))
 for PICKED_OBS_ID in $OBS_ID; do
 
   if [ $OBS_ID != all ]; then
-    QUERY_FOR_STACK=$QUERY"&&Data.ObservationId==$OBS_ID"
-    QUERY_FOR_CALIB=$QUERY"&&Data.ObservationSequence.ObservationId==$OBS_ID"
+    QUERY_FOR_STACK=$QUERY"&&Data.ObservationId==$PICKED_OBS_ID"
+    QUERY_FOR_CALIB=$QUERY"&&Data.ObservationSequence.ObservationId==$PICKED_OBS_ID"
   else
     QUERY_FOR_STACK=$QUERY
     QUERY_FOR_CALIB=$QUERY
   fi
 
-  echo "Query: $QUERY_FOR_OBS"
+  echo "Query: $QUERY_FOR_STACK"
 
   # Get the DpdVisStackedFrame product and fits files
   CMD='python '$BASEDIR'/dataProductRetrieval_SC8.py --username '`cat $HOME/.username.txt`' --password '`cat $HOME/.password.txt`' --project TEST --data_product DpdVisStackedFrame --query "'$QUERY_FOR_STACK'"'
   echo "Command: $CMD"
   eval $CMD
+
+  echo "Query: $QUERY_FOR_CALIB"
 
   # Get the DpdVisCalibratedFrame product and fits files
   CMD='python '$BASEDIR'/dataProductRetrieval_SC8.py --username '`cat $HOME/.username.txt`' --password '`cat $HOME/.password.txt`' --project TEST --data_product DpdVisCalibratedFrame --query "'$QUERY_FOR_CALIB'"'
