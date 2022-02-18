61a83ba29e84418b7c97e182b042fa4c11ef35e0
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2022-02-18 14:12:22 +0000

    Fixing project name in echoed run commands

--- SHE_Pipeline/python/SHE_Pipeline/RunPipeline.py
+++ SHE_Pipeline/python/SHE_Pipeline/RunPipeline.py
@@ -22,10 +22,9 @@ __updated__ = "2020-07-30"
 
 import argparse
 
-from EL_PythonUtils.utilities import get_arguments_string
-
-from ElementsKernel.Logging import getLogger
 import SHE_Pipeline
+from EL_PythonUtils.utilities import get_arguments_string
+from ElementsKernel.Logging import getLogger
 from SHE_Pipeline.run_pipeline import run_pipeline_from_args
 
 
@@ -47,37 +46,38 @@ def defineSpecificProgramOptions():
     parser = argparse.ArgumentParser()
 
     # Input arguments
-    parser.add_argument('--pipeline', type=str,
-                        help='Name of the pipeline (e.g. "sensitivity_testing")')
-    parser.add_argument('--isf', type=str,
-                        help='Fully-qualified name of input specification file for the pipeline')
-    parser.add_argument('--isf_args', type=str, nargs='*',
-                        help='Additional arguments to write to the ISF (must be in pairs of port_name file_name)')
-    parser.add_argument('--config', type=str,
-                        help='Fully-qualified name of pipeline config file for this run')
-    parser.add_argument('--config_args', type=str, nargs='*',
-                        help='Additional arguments to write to the pipeline_config (must be in pairs of key value)')
-    parser.add_argument('--serverurl', type=str, default=None)
-    parser.add_argument('--server_config', type=str,
-                        default=None,
-                        help="The name of the server configuration file to use.")
-    parser.add_argument('--use_debug_server_config', action="store_true",
-                        help="If set, will use a server configuration file which outputs all logs to stdout, " +
-                        "overriding any provided to the --server_config option.")
-    parser.add_argument('--cluster', action='store_true',
-                        help='Necessary if running on a cluster, causing the pipeline to be executed by another user.')
-    parser.add_argument('--dry_run', action='store_true',
-                        help="If set, will do everything except actually call the pipeline - useful for testing.")
-    parser.add_argument('--skip_file_setup', action='store_true',
-                        help="If set, will not try to sort out issues with file locations " +
-                        "or move AUX files to the work directory.")
+    parser.add_argument('--pipeline', type = str,
+                        help = 'Name of the pipeline (e.g. "sensitivity_testing")')
+    parser.add_argument('--isf', type = str,
+                        help = 'Fully-qualified name of input specification file for the pipeline')
+    parser.add_argument('--isf_args', type = str, nargs = '*',
+                        help = 'Additional arguments to write to the ISF (must be in pairs of port_name file_name)')
+    parser.add_argument('--config', type = str,
+                        help = 'Fully-qualified name of pipeline config file for this run')
+    parser.add_argument('--config_args', type = str, nargs = '*',
+                        help = 'Additional arguments to write to the pipeline_config (must be in pairs of key value)')
+    parser.add_argument('--serverurl', type = str, default = None)
+    parser.add_argument('--server_config', type = str,
+                        default = None,
+                        help = "The name of the server configuration file to use.")
+    parser.add_argument('--use_debug_server_config', action = "store_true",
+                        help = "If set, will use a server configuration file which outputs all logs to stdout, " +
+                               "overriding any provided to the --server_config option.")
+    parser.add_argument('--cluster', action = 'store_true',
+                        help = 'Necessary if running on a cluster, causing the pipeline to be executed by another '
+                               'user.')
+    parser.add_argument('--dry_run', action = 'store_true',
+                        help = "If set, will do everything except actually call the pipeline - useful for testing.")
+    parser.add_argument('--skip_file_setup', action = 'store_true',
+                        help = "If set, will not try to sort out issues with file locations " +
+                               "or move AUX files to the work directory.")
 
     # Input arguments for the bias measurement pipeline
-    parser.add_argument('--plan_args', type=str, nargs='*',
-                        help='Arguments to write to simulation plan (must be in pairs of key value)')
+    parser.add_argument('--plan_args', type = str, nargs = '*',
+                        help = 'Arguments to write to simulation plan (must be in pairs of key value)')
 
-    parser.add_argument('--workdir', type=str,)
-    parser.add_argument('--logdir', type=str,)
+    parser.add_argument('--workdir', type = str, )
+    parser.add_argument('--logdir', type = str, )
 
     logger.debug('# Exiting SHE_Pipeline_Run defineSpecificProgramOptions()')
 
@@ -100,9 +100,10 @@ def mainMethod(args):
     logger.debug('# Entering SHE_Pipeline_Run mainMethod()')
     logger.debug('#')
 
-    exec_cmd = get_arguments_string(args, cmd="E-Run SHE_Pipeline " + SHE_Pipeline.__version__ + " SHE_Pipeline_Run",
-                                    store_true=["profile", "debug", "cluster", "use_debug_server_config",
-                                                "dry_run", "skip_file_setup"])
+    exec_cmd = get_arguments_string(args, cmd = "E-Run SHE_IAL_Pipelines " + SHE_Pipeline.__version__ + " "
+                                                                                                        "SHE_Pipeline_Run",
+                                    store_true = ["profile", "debug", "cluster", "use_debug_server_config",
+                                                  "dry_run", "skip_file_setup"])
     logger.info('Execution command for this step:')
     logger.info(exec_cmd)
 
